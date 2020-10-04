e99bbf32d0b3d4e6a20ab0e11fdac34d851ec977
meyjac16 <jmeyer32@wisc.edu>
2020-10-04 15:22:51 -0500

    Changed to corn field idea

--- drawing.js
+++ drawing.js
@@ -12,30 +12,6 @@ function setup() {
             context.setTransform(Tx[0], Tx[1], Tx[3], Tx[4], Tx[6], Tx[7]);
         }
 
-        function drawTrunk() {
-            context.beginPath();
-            context.fillStyle = "#824403";
-            context.lineWidth = 1;
-            context.moveTo(0, 0);
-            context.lineTo(10, -15);
-            context.lineTo(10, -50);
-            context.lineTo(40, -50);
-            context.lineTo(40, -15);
-            context.lineTo(50, 0);
-            context.closePath();
-            context.fill();
-            context.stroke();
-        }
-
-        function drawTree() {
-
-        }
-
-        function tree() {
-            drawTrunk();
-            drawTree();
-        }
-
         function drawBackground() {
             context.beginPath();
             context.fillStyle = "#2cbdf2";
@@ -43,18 +19,85 @@ function setup() {
             context.closePath();
             context.fill();
             context.beginPath();
-            context.fillStyle = "#168f09";
+            context.fillStyle = "#997950";
             context.rect(0, 450, canvas.width, 250);
             context.closePath();
             context.fill();
         }
 
+        function drawStalk(broken) {
+            if (broken === true) {
+                context.beginPath();
+                context.fillStyle = "#a0a653";
+                context.lineWidth = 1;
+                context.moveTo(0, 0);
+                context.lineTo(0, -30);
+                context.lineTo(10, -28);
+                context.lineTo(10, 0);
+                context.closePath();
+                context.fill();
+                context.stroke();
+            } else {
+                context.beginPath();
+                context.fillStyle = "#a0a653";
+                context.lineWidth = 1;
+                context.moveTo(0, 0);
+                context.lineTo(0, -210);
+                context.lineTo(10, -210);
+                context.lineTo(10, 0);
+                context.closePath();
+                context.fill();
+                context.stroke();
+
+                var cobs = mat3.create();
+                mat3.fromTranslation(cobs, [10, -70]);
+                var Tx_curr = context.getTransform();
+                var cobs_to_canvas = mat3.create();
+                mat3.multiply(cobs_to_canvas, cobs, Tx_curr);
+                setCanvasTransform(cobs_to_canvas);
+                drawCob();
+            }
+        }
+
+        function drawCob() {
+            context.beginPath();
+            context.fillStyle = "#e2e62c";
+            context.lineWidth = 1;
+            context.ellipse(0, 0, 25, 10,
+                Math.PI/2, 0, 2 * Math.PI);
+            context.closePath();
+            context.fill();
+            context.stroke();
+            // Leaves
+            context.beginPath();
+            context.fillStyle = "#3a7037";
+            context.moveTo(-10, 30);
+            context.lineTo(15, 0);
+            context.lineTo(10, 30);
+            context.closePath();
+            context.fill();
+            context.stroke();
+            context.moveTo(10, 30);
+            context.lineTo(-10, 30);
+            context.lineTo(-15, 0);
+            context.closePath();
+            context.fill();
+            context.stroke();
+        }
+
+        function corn(Tx_curr, broken) {
+            drawStalk(broken);
+            // Bring canvas back to wherever it
+            // started before drawing the corn
+            setCanvasTransform(Tx_curr);
+        }
+
         drawBackground();
 
-        var firstTree = mat3.create();
-        mat3.fromTranslation(firstTree, [300, 600]);
-        setCanvasTransform(firstTree);
-        tree();
+        var firstCorn = mat3.create();
+        mat3.fromTranslation(firstCorn, [600, 600]);
+        setCanvasTransform(firstCorn);
+        corn(firstCorn, false);
     }
 
     draw();
