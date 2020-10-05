7aa023ae9fc754444c7f0cd94e813a586e34f20c
meyjac16 <jmeyer32@wisc.edu>
2020-10-05 16:28:33 -0500

    Makin da wheels spin

--- drawing.js
+++ drawing.js
@@ -4,6 +4,7 @@ function setup() {
     var canvas = document.getElementById("myCanvas");
     var mat3 = glMatrix.mat3;
     var theta = 0;
+    var phi = 0;
     var x = 0;
 
     function draw() {
@@ -85,7 +86,6 @@ function setup() {
                 mat3.multiply(stack[0], stack[0], cob4);
                 setCanvasTransform(stack[0]);
                 drawCob();
-                stack.shift();
             }
         }
 
@@ -125,12 +125,115 @@ function setup() {
 
         function combine_() {
             stack.unshift(mat3.clone[0]);
+            // Arm
+            context.beginPath();
+            context.fillStyle = "#a60c0c";
+            context.lineWidth = 1;
+            context.moveTo(270, -50);
+            context.lineTo(335, 10);
+            context.lineTo(410, 10);
+            context.lineTo(270, -80);
+            context.closePath();
+            context.fill();
+            context.stroke();
+
             // Body
             context.beginPath();
-            context.fillStyle = "#e32929";
-            context.lineWidth = 1;
+            context.fillStyle = "#ed0000";
             context.moveTo(0, 0);
-            context.lineTo(0, -70);
+            context.lineTo(0, -40);
+            context.lineTo(-40, -40);
+            context.lineTo(0, -180);
+            context.lineTo(280, -200);
+            context.lineTo(320, -60);
+            context.lineTo(270, -60);
+            context.lineTo(270, 0);
+            context.closePath();
+            context.fill();
+            context.stroke();
+
+            // Window
+            context.beginPath();
+            context.fillStyle = "#96ffff";
+            context.moveTo(310, -70);
+            context.lineTo(270, -190);
+            context.lineTo(210, -185);
+            context.lineTo(210, -70);
+            context.closePath();
+            context.fill();
+            context.stroke();
+
+            // Tube
+            context.beginPath();
+            context.fillStyle = "#a60c0c";
+            context.moveTo(180, -170);
+            context.lineTo(-80, -170);
+            context.lineTo(-95, -130);
+            context.lineTo(-60, -130);
+            context.lineTo(-60, -150);
+            context.lineTo(180, -150);
+            context.closePath();
+            context.fill();
+            context.stroke();
+
+
+            // Wheels
+            stack.unshift(mat3.clone(stack[0]));
+            var wheel_spin = mat3.create();
+            mat3.fromTranslation(wheel_spin, [50, 0]);
+            mat3.rotate(wheel_spin, wheel_spin, phi);
+            mat3.multiply(stack[0], stack[0], wheel_spin);
+            context.beginPath();
+            context.fillStyle = 'black';
+            context.arc(50, 0, 50, 0, 2 * Math.PI);
+            context.fill();
+            context.stroke();
+            context.arc(270, 0, 60, 0, 2 * Math.PI);
+            context.fill();
+            context.stroke();
+            context.closePath();
+            context.beginPath();
+            context.fillStyle = "#c4c4c4";
+            context.moveTo(50, 0);
+            context.arc(50, 0, 25, 0, Math.PI/2);
+            context.closePath();
+            context.fill();
+            context.beginPath();
+            context.moveTo(50, 0);
+            context.arc(50, 0, 25, Math.PI, 3*Math.PI/2);
+            context.closePath();
+            context.fill();
+            context.beginPath();
+            context.moveTo(270, 0);
+            context.arc(270, 0, 35, 0, Math.PI/2);
+            context.closePath();
+            context.fill();
+            context.beginPath();
+            context.moveTo(270, 0);
+            context.arc(270, 0, 35, Math.PI, 3*Math.PI/2);
+            context.closePath();
+            context.fill();
+            context.beginPath();
+            context.fillStyle = "#5e5e5e";
+            context.moveTo(50, 0);
+            context.arc(50, 0, 25, 3*Math.PI/2, 2 * Math.PI);
+            context.closePath();
+            context.fill();
+            context.beginPath();
+            context.moveTo(50, 0);
+            context.arc(50, 0, 25, Math.PI/2, Math.PI);
+            context.closePath();
+            context.fill();
+            context.beginPath();
+            context.moveTo(270, 0);
+            context.arc(270, 0, 35, 3*Math.PI/2, 2 * Math.PI);
+            context.closePath();
+            context.fill();
+            context.beginPath();
+            context.moveTo(270, 0);
+            context.arc(270, 0, 35, Math.PI/2, Math.PI);
+            context.closePath();
+            context.fill();
             stack.shift();
         }
 
@@ -139,26 +242,32 @@ function setup() {
         // Draw the corn
         stack.unshift(mat3.clone(stack[0]));
         var firstCorn = mat3.create();
-        mat3.fromTranslation(firstCorn, [600, 600]);
+        mat3.fromTranslation(firstCorn, [700, 600]);
         mat3.multiply(stack[0], stack[0], firstCorn);
         setCanvasTransform(stack[0]);
         corn(false);
         stack.shift();
 
-        /*
         stack.unshift(mat3.clone(stack[0]));
         var secondCorn = mat3.create();
-        mat3.fromTranslation(secondCorn, [700, 600]);
+        mat3.fromTranslation(secondCorn, [800, 600]);
         mat3.multiply(stack[0], stack[0], secondCorn);
         setCanvasTransform(stack[0]);
         corn(false);
         stack.shift();
-         */
+
+        stack.unshift(mat3.clone(stack[0]));
+        var thirdCorn = mat3.create();
+        mat3.fromTranslation(thirdCorn, [900, 600]);
+        mat3.multiply(stack[0], stack[0], thirdCorn);
+        setCanvasTransform(stack[0]);
+        corn(false);
+        stack.shift();
 
         // Draw the combine
         stack.unshift(mat3.clone(stack[0]));
         var combine = mat3.create();
-        mat3.fromTranslation(combine, [200, 600]);
+        mat3.fromTranslation(combine, [100, 580]);
         mat3.multiply(stack[0], stack[0], combine);
         setCanvasTransform(stack[0]);
         combine_();
